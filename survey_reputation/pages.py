4106b40668c3923df5bfe50e410a5a834fe08047
Shahin Baghirov <shahin.baghirov@gmail.com>
2023-05-01 22:40:56 +0300

    Initial commit

---
+++ survey_reputation/pages.py
@@ -0,0 +1,37 @@
+from otree.api import Currency as c, currency_range
+
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+class Intro(Page):
+    pass
+
+class Risk(Page):
+    form_model = 'player'
+    form_fields = ['risk',
+                   'loss1',
+                   'loss2']
+
+class Demographics(Page):
+    form_model = 'player'
+    form_fields = ['age',
+                   'gender',
+                   'year',
+                   'dept',
+                   'scholarship',
+                   'father',
+                   'mother',
+                   'experiment',
+                   'tercih',
+                   'tercih_2',
+                   'tercih_3',
+                   ]
+
+
+
+page_sequence = [
+    Intro,
+    Risk,
+    Demographics,
+   
+]
